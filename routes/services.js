1f82998a5fd2cdeb73014af61e75d35960e18f0f
Michael Hampson <toxotes@gmail.com>
2022-04-05 08:45:56 -0400

    CORS and preflight headers

--- routes/services.js
+++ routes/services.js
@@ -6,6 +6,17 @@ const flash = require('connect-flash');
 const router = express.Router();
 router.use(flash());
 
+router.use((req, res, next) => {
+    res.set({
+        'Access-Control-Allow-Origin':'*',
+        'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type',
+    });
+    if(req.method == 'OPTIONS') {
+      return res.status(200).end();
+    }
+    next();
+})
 
 router.get('/', async (req, res, next) => {
     res.render('main', {
